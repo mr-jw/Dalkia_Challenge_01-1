9b72c83dc92988a28c417b26f37951572d4676c1
mr-jw <joshua.walley01@gmail.com>
2025-04-09 15:41:28 +0100

    Refactor Javascript

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
-function f1() {
-    var p = new Date();
-    var q = new Date("2025-12-25");
+function getCurrentDate() {
+        return new Date();
+}
+
+function getSpecificDate(date) {
+    if (validateDateArgument(date)) {
+        return new Date(date);
+    }
+}
+
+function validateDateArgument(date)
+{
+    // validate data type.
+    if (typeof date !== "string") {
+        console.error("Argument must be string.");
+        return false;
+    }
+
+    // validate string format.
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateRegex.test(date)) {
+        console.error("Date must be in YYYY-MM-DD format.");
+        return false;
+    }
+
+  return true;
+}
+
+function calculateRemainingDays(currentDate, targetDate) {
+    return Math.round((targetDate - currentDate) / (1000 * 60 * 60 * 24));
+}
+
+function alterPageElement(result, elementId) {
+    var variable = document.getElementById(elementId);
+    variable.innerText = result;
+}
+
+function f3(date, elementId) {
+    // dates.
+    var p = getCurrentDate();
+    var q = getSpecificDate(date); 
   
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element");
-    variable2.innerText = variable;
+    // result of calculation.
+    var result = calculateRemainingDays(p, q);
+
+    // alter specified page element.
+    alterPageElement(result, elementId);
   }
-  
-  function f2() {
-    var p = new Date();
-    var q = new Date("2026-01-01");
-  
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element2");
-    variable2.innerText = variable;
-  }
-  
-  f1();
-  f2();
+
+  f3("2025-12-25", "my-element");
+  f3("2026-01-01", "my-element2");
   
